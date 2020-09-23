0b26cf31c54569de333754b6cda70098f1a199c5
Vitaliy Nikolaiev <helicoptero3345@gmail.com>
2020-09-23 11:22:07 +0300

    add settings.sass and grid.sass

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,25 @@ const {src, dest, watch} = require("gulp");
 const browserSync = require("browser-sync").create();
 const sass = require("gulp-sass");
 
-function serveSass() {
-  return src('src/sass/**/*.sass', 'src/sass/**/*.scss')
-  .pipe(sass())
-  .pipe(dest("src/style"))
-  .pipe(browserSync.stream());
-};
-
 // Static server
 function bs() {
   serveSass();
   browserSync.init({
     server: {
-      baseDir: "src",
+      baseDir: "src/",
     },
   });
   watch("src/*.html").on("change", browserSync.reload);
   watch("src/sass/**/*.sass", serveSass);
   watch("src/sass/**/*.scss", serveSass);
+  watch("src/script/*.js").on("change", browserSync.reload);
+};
+
+function serveSass() {
+  return src('src/sass/**/*.sass', 'src/sass/**/*.scss')
+    .pipe(sass())
+    .pipe(dest("./src/style"))
+    .pipe(browserSync.stream());
 };
 
 exports.serve = bs;
