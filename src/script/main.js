ce09e61ff41d052c39c2a587676cdbde4df6be9b
Vitaliy Nikolaiev <helicoptero3345@gmail.com>
2020-09-28 10:45:22 +0300

    fixed team & team toggle

--- src/script/main.js
+++ src/script/main.js
@@ -1,19 +1,19 @@
 const accordion = () => {
   let header = document.querySelectorAll(".accordion__header"),
     content = document.querySelectorAll(".accordion__content");
-    
-  for(let i = 0; i < header.length; i++) {
-    header[i].addEventListener('click', () => {
-      content[i].style.transition = 'all 0.3s ease'
-      if(content[i].style.height) {
+
+  for (let i = 0; i < header.length; i++) {
+    header[i].addEventListener("click", () => {
+      content[i].style.transition = "all 0.3s ease";
+      if (content[i].style.height) {
         content[i].style.height = null;
       } else {
-        for(let j = 0; j < header.length; j++) {
+        for (let j = 0; j < header.length; j++) {
           content[j].style.height = null;
         }
         content[i].style.height = `${content[i].scrollHeight}px`;
       }
-    })
+    });
   }
 };
 
@@ -26,7 +26,7 @@ const slider = () => {
 
   let count = 0;
   let marg = getComputedStyle(item[0]);
-  let marginBot = parseInt(marg.marginBottom); 
+  let marginBot = parseInt(marg.marginBottom);
   let size = item[0].clientHeight + marginBot;
 
   container.style.transform = `translateY(${-size * count}px)`;
@@ -35,12 +35,12 @@ const slider = () => {
     container.style.transform = `translateY(${-size * count}px)`;
     container.style.transition = `transform 0.3s ease`;
   };
-  
-  btn.forEach(el => {
-    el.addEventListener('click', event => {
+
+  btn.forEach((el) => {
+    el.addEventListener("click", (event) => {
       let target = event.target;
       if (target.matches(".fa-angle-up")) {
-        if(count >= item.length - 2) {
+        if (count >= item.length - 2) {
           return;
         }
         count++;
@@ -52,8 +52,8 @@ const slider = () => {
         count--;
       }
       switcher();
-    })
-  })
+    });
+  });
 };
 
 slider();
@@ -62,23 +62,25 @@ const toggler = () => {
   const image = document.querySelectorAll(".team__slider-image"),
     name = document.querySelectorAll(".team__name");
 
-
-
-    for(let i = 0; i < image.length; i++) {
-      image[i].addEventListener('mouseover', (event) => {
-        let target = event.target;
-        if (image[i].classList.contains("team__slider-image")) {
-          image[i].classList.toggle("team__slider-image--active");
-          // name[i].classList.toggle("team__name--active");
-          for (let j = 0; j < image.length; j++) {
-            if (!image[i]) {
-              image[j].classList.toggle("team__slider-image--active", false);
-              // name[j].classList.toggle("team__name--active", false);
-            }
-          }
-        } 
-      });
-    }
+  for(let j = 0; j < image.length; j++) {
+    image[j].addEventListener('click', (event) => {
+      event.preventDefault();
+      // if (
+      //   image[j].matches(".team__slider-image--active") && 
+      //   image[j].matches(".team__slider-image")
+      // ) {
+      //   // image[j].classList.remove("team__slider-image--active");
+      //   // name[j].classList.remove("team__name--active");
+      // } else {
+        for (let i = 0; i < image.length; i++) {
+          image[i].classList.remove("team__slider-image--active");
+          name[i].classList.remove("team__name--active");
+        }
+        image[j].classList.add("team__slider-image--active");
+        name[j].classList.add("team__name--active");
+      // }
+    });
+  }
 };
 
 toggler();
